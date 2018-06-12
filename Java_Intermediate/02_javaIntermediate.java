38f548a04b4032b5034dce73676fd93ad9f412be
Jason Siu <34889726+Jason-Siu@users.noreply.github.com>
2018-06-12 16:45:34 -0500

    Update 02_javaIntermediate.java
    Add comments to describe what was going on

--- Java_Intermediate/02_javaIntermediate.java
+++ Java_Intermediate/02_javaIntermediate.java
@@ -1,23 +1,30 @@
 public class Main {
 
     public static void main(String[] args) {
-
+    
+        // initialize string
         String s = "buckyrobertsbuckyroberts";
-
+        
+        // prints the index of first occurence of "rob"
         System.out.println(s.indexOf("rob", 10));
-
+        
         String a = "BACON ";
         String b = "monster";
         String c = "     flying        ";
-
+        
+        // prints a and b combined
         System.out.println(a.concat(b));
         
+        // prints replaced string, but doesn't change actual reference
         System.out.println(a.replace('B', 'F'));
         
+        // everything all uppcase letters (only affects letters)
         System.out.println(b.toUpperCase());
-        
+           
+        // lower case version. It will not convert lower to upper
         System.out.println(a.toLowerCase());
         
+        // gets rid of unnecessary spaces from left to right
         System.out.println(c.trim());
 
     }
