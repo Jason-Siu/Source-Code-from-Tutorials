c87ab03092aa5a88f95ce8c7c06875f0aee38fd3
Jason Siu <34889726+Jason-Siu@users.noreply.github.com>
2018-06-12 01:18:27 -0500

    Update 01_javaIntermediate.java

--- Java_Intermediate/01_javaIntermediate.java
+++ Java_Intermediate/01_javaIntermediate.java
@@ -1,11 +1,16 @@
 public class Main {
-
+    
+    // create main method
     public static void main(String[] args) {
-
+        
+        // initialize string static array with intializer list
         String[] words = { "funk", "chunk", "furry", "baconator" };
 
         //startsWith
+        // iterates through the array using enhanced for loop
+        // note that w is just a copy of the value - not the reference itself
         for (String w: words) {
+            // if it starts with fu, print string
             if (w.startsWith("fu")) {
                 System.out.println(w + " starts with fu");
             }
